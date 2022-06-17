44019e4d1d4fe89bac95db758e3eee4474456930
Kirill <3025889+kirillsv7@users.noreply.github.com>
2022-06-17 16:50:57 +0200

    Vue product show

--- resources/js/components/Product/Index.vue
+++ resources/js/components/Product/Index.vue
@@ -1,30 +1,32 @@
 <template>
   <div class="card">
     <div class="card-body">
-      <div class="container">
-        <table class="table table-borderless">
-          <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Price</th>
-            <th></th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr v-for="product in products">
-            <td>{{ product.id }}</td>
-            <td>{{ product.name }}</td>
-            <td>{{ product.description }}</td>
-            <td>{{ product.price }}</td>
-            <td>
-              <button class="btn btn-sm btn-close" @click="deleteProduct(product.id)"></button>
-            </td>
-          </tr>
-          </tbody>
-        </table>
-      </div>
+      <table class="table table-borderless">
+        <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Price</th>
+          <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="product in products">
+          <td>{{ product.id }}</td>
+          <td>
+            <router-link :to="{name: 'product.show', params: {id: product.id}}">
+              {{ product.name }}
+            </router-link>
+          </td>
+          <td>{{ product.description }}</td>
+          <td>{{ product.price }}</td>
+          <td>
+            <button class="btn btn-sm btn-close" @click="deleteProduct(product.id)"></button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
--- resources/js/components/Product/Show.vue
+++ resources/js/components/Product/Show.vue
@@ -1,3 +1,58 @@
 <template>
-  Product show
+  <div class="card">
+    <div class="card-body">
+      <table class="table table-borderless">
+        <tbody>
+        <tr>
+          <th scope="row">ID</th>
+          <td>{{ product.id }}</td>
+        </tr>
+        <tr>
+          <th scope="row">Name</th>
+          <td>{{ product.name }}</td>
+        </tr>
+        <tr>
+          <th scope="row">Description</th>
+          <td>{{ product.description }}</td>
+        </tr>
+        <tr>
+          <th scope="row">Price</th>
+          <td>{{ product.price }}</td>
+        </tr>
+        <tr>
+          <th scope="row">Created</th>
+          <td>{{ product.created_at }}</td>
+        </tr>
+        <tr>
+          <th scope="row">Updated</th>
+          <td>{{ product.updated_at }}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
+
+<script>
+import useProduct from "../../composable/product";
+import {onMounted} from "vue";
+
+export default {
+  props: {
+    id: {
+      required: true,
+      type: String
+    }
+  },
+
+  setup(props) {
+    const {product, getProduct} = useProduct()
+
+    onMounted(getProduct(props.id))
+
+    return {
+      product
+    }
+  }
+}
+</script>
