b71a5be3760ded56ff41ac0a03f21f12aa59d19f
Kirill <3025889+kirillsv7@users.noreply.github.com>
2022-06-17 16:58:54 +0200

    Vue product create

--- resources/js/components/Product/Create.vue
+++ resources/js/components/Product/Create.vue
@@ -1,3 +1,40 @@
 <template>
-  Product create
+  <div class="card">
+    <div class="card-body">
+      <table class="table table-borderless">
+        <tbody>
+        <tr>
+          <th scope="row">Name</th>
+          <td><input class="form-control" v-model="product.name"></td>
+        </tr>
+        <tr>
+          <th scope="row">Description</th>
+          <td><textarea class="form-control" v-model="product.description"/></td>
+        </tr>
+        <tr>
+          <th scope="row">Price</th>
+          <td><input class="form-control" v-model="product.price"></td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="card-footer">
+      <button class="btn btn-primary" @click="storeProduct()">Save</button>
+    </div>
+  </div>
 </template>
+
+<script>
+import useProduct from "../../composable/product";
+
+export default {
+  setup() {
+    const {product, storeProduct} = useProduct()
+
+    return {
+      product,
+      storeProduct
+    }
+  }
+}
+</script>
--- resources/js/components/Product/Index.vue
+++ resources/js/components/Product/Index.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="card">
+    <div class="card-header">
+      <router-link class="btn btn-primary" :to="{name: 'product.create'}">Add product</router-link>
+    </div>
     <div class="card-body">
       <table class="table table-borderless">
         <thead>
         <tr>
           <th>ID</th>
-          <th>Name</th>
-          <th>Description</th>
+          <th class="w-25">Name</th>
+          <th class="w-50">Description</th>
           <th>Price</th>
           <th></th>
         </tr>
