5bfa082d7737eb6b89bad9436e55d70fca23de47
Kirill <3025889+kirillsv7@users.noreply.github.com>
2022-06-17 16:16:38 +0200

    Product composable

---
+++ resources/js/composable/product.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+import {ref} from "vue";
+import {useRouter} from "vue-router"
+
+export default function useProduct() {
+
+    const router = useRouter()
+    const product = ref({})
+    const products = ref([])
+    const errors = ref({})
+
+    const getProducts = async () => {
+        products.value = (await axios.get('/api/product')).data.data
+    }
+
+    const storeProduct = async () => {
+        errors.value = {}
+        try {
+            await axios.post('/api/product', product.value)
+            await router.push({name: 'product.index'})
+        } catch (e) {
+            await handleException(e)
+        }
+    }
+
+    const getProduct = async (id) => {
+        product.value = (await axios.get(`/api/product/${id}`)).data.data
+    }
+
+    const updateProduct = async (id) => {
+        errors.value = {}
+        try {
+            await axios.put(`/api/product/${id}`, product.value)
+            await router.push({name: 'product.edit'})
+        } catch (e) {
+            await handleException(e)
+        }
+    }
+
+    const deleteProduct = async (id) => {
+        if (!window.confirm('Are you sure you want to delete?'))
+            return
+        try {
+            await axios.delete(`/api/product/${id}`)
+            await getProducts()
+        } catch (e) {
+            await handleException(e)
+        }
+    }
+
+    const handleException = (e) => {
+        switch (e.response.status) {
+            case 422:
+                errors.value = e.response.data.errors
+                break
+            case 403:
+                break
+            default:
+                console.log(e.response)
+                break
+        }
+    }
+
+    return {
+        product,
+        products,
+        getProducts,
+        storeProduct,
+        getProduct,
+        updateProduct,
+        deleteProduct,
+        errors
+    }
+}
