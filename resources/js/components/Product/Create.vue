5a35ce41c551288514dab09f10cd87fa63ecb35f
Kirill <3025889+kirillsv7@users.noreply.github.com>
2022-06-17 17:13:18 +0200

    Vue product create reuse Form partial

--- resources/js/components/Product/Create.vue
+++ resources/js/components/Product/Create.vue
@@ -1,22 +1,7 @@
 <template>
   <div class="card">
     <div class="card-body">
-      <table class="table table-borderless">
-        <tbody>
-        <tr>
-          <th scope="row">Name</th>
-          <td><input class="form-control" v-model="product.name"></td>
-        </tr>
-        <tr>
-          <th scope="row">Description</th>
-          <td><textarea class="form-control" v-model="product.description"/></td>
-        </tr>
-        <tr>
-          <th scope="row">Price</th>
-          <td><input class="form-control" v-model="product.price"></td>
-        </tr>
-        </tbody>
-      </table>
+      <Form :product="product"/>
     </div>
     <div class="card-footer">
       <button class="btn btn-primary" @click="storeProduct()">Save</button>
@@ -25,9 +10,11 @@
 </template>
 
 <script>
+import Form from "./Partials/Form";
 import useProduct from "../../composable/product";
 
 export default {
+  components: {Form},
   setup() {
     const {product, storeProduct} = useProduct()
 
